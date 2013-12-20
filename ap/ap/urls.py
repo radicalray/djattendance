a887b9f4a6c61840d7d6e716afd3a88efd3332e1
aellis24 <zamirellis19@gmail.com>
2013-12-20 15:40:53 -0800

    Added urls links in ap/urls.py

--- ap/ap/urls.py
+++ ap/ap/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
+#from reports.views import RosterTemplate
 
-# Uncomment the next two lines to enable the admin:
+#Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 import autofixture
 
@@ -13,8 +14,16 @@ urlpatterns = patterns('',
     url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', logout),
     url(r'^$', 'ap.views.home'),
-
-    url(r'^terms/', include('terms.urls', namespace="terms")),
+   	
+    #url(r'^roster/$','reports.views.roster'),
+       
+    #url(r'^terms/', include('terms.urls', namespace="terms")),
+    url(r'^reports/', include('reports.urls')),
+    # url(r'^roster/$', 'reports.views.Roster'),
+    # url(r'^roster_temp/$', 'reports.views.Roster_Temp'),
+    # url(r'^roster_simple/$', 'reports.views.Roster_Temp_simple'),
+    # url(r'^roster_class_view/$', RosterTemplate.as_view()),
+    
     # Examples:
     # url(r'^$', 'ap.views.home', name='home'),
     # url(r'^ap/', include('ap.foo.urls')),
