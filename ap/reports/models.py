6e9bf05f3bad638de4cf8d0ff00ba59f5fa54892
Zuriel <mjfolwarski@gmail.com>
2013-12-22 21:35:01 -0800

    Modified the Trainee reports table to display only the correct fields
    *populating these fields is still in progress. Term is returning a term
    object so we'll need to look into how we will handle this.

--- ap/reports/models.py
+++ ap/reports/models.py
@@ -2,5 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Trainee(models.Model):
-    name = models.CharField(verbose_name="full name", max_length=50)
+class TraineeReport(models.Model):
+    hc = models.BooleanField(default=False)
+    name= models.CharField(verbose_name="Name", max_length=50)
+    gender= models.CharField(verbose_name="G", max_length=1)
+    term= models.CharField(verbose_name="TM", max_length=1)
+    locality= models.CharField(verbose_name="Locality", max_length=50)
+    team= models.CharField(verbose_name="Team", max_length=50)
+    residence= models.CharField(verbose_name="Residence", max_length=50)
+    bed= models.CharField(verbose_name="bed", max_length=50)
+    v= models.BooleanField(verbose_name="V", default=False)
+    cell= models.CharField(verbose_name="Cell P", max_length=50)
+    TA= models.CharField(verbose_name="", max_length=50)
+
--- ap/reports/tables.py
+++ ap/reports/tables.py
@@ -1,8 +1,9 @@
 import django_tables2 as tables
-from accounts.models import Trainee
+from reports.models import TraineeReport
 
 class TraineeTable(tables.Table):
     class Meta:
-        model = Trainee
+        model = TraineeReport
         # add class="paleblue" to <table> tag
         attrs = {"class": "paleblue"}
+        exclude = ("ID",)
